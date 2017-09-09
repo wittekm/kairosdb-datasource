7d4930fca677e7f8aa6b4f1bb0ac9077e3bb7830
Brandon Arp <brandonarp@gmail.com>
2017-09-09 10:29:09 -0700

    clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,17 @@
 'use strict';
 module.exports = function(grunt) {
-grunt.loadNpmTasks('grunt-typescript');
 
-grunt.initConfig({
-ts: {
-    default: {
-      src: ["**/*.ts", "!node_modules/**"],
-      tsconfig: true,
-      options: {
-        verbose: true
+  grunt.initConfig({
+    ts: {
+      default: {
+        src: ["**/*.ts", "!node_modules/**"],
+        tsconfig: true,
+        options: {
+          verbose: true
+        }
       }
     }
-  }
-});
+  });
 
   grunt.loadNpmTasks("grunt-ts");
   grunt.registerTask("default", ["ts"]);
